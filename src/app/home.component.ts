6dba7f384508815a07d35d0acc30caef487ce572
Laurence Bugasu Ininda <laudebugs@gmail.com>
2023-10-02 14:58:04 -0400

    update 2

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -5,6 +5,7 @@ import { Component } from "@angular/core";
   template: ` <h1>Home</h1>
   <!-- Something about home -->
   <p>Home is where the heart is</p>
+  <img src="https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1587&q=80" width="300" alt="">
   `
 })
 export class HomeComponent {
