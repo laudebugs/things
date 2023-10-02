59e3de644b5d55e461b5c5a902aaaf573e303a5e
Laurence Bugasu Ininda <laudebugs@gmail.com>
2023-10-02 14:31:28 -0400

    add home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FoodComponent } from './food.component';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path:'food',
    component: FoodComponent
   },
+  {
+    path:'home',
+   component: HomeComponent
+  },
+  {
+    path:'**',
+   component: HomeComponent
+  }
 ];
 
 @NgModule({
