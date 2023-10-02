2619850be4a71a0ce2996c7b7a559be4155f0239
Laurence Bugasu Ininda <laudebugs@gmail.com>
2023-10-02 13:59:03 -0400

    revert app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,11 @@ import { UpdateService } from './update.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  #collapsed = JSON.parse(localStorage.getItem('collapsed') ?? 'false')
+  title = 'things';
 
   constructor(private updateService: UpdateService){}
-
-  get collapsed(): boolean {
-      return this.#collapsed
-  }
-  set collapsed(value: boolean) {
-      this.#collapsed = value
-      localStorage.setItem('collapsed', JSON.stringify(value))
-  }
-
-  toggleCollapse(): void {
-      this.collapsed = !this.collapsed
-  }
 }
+
