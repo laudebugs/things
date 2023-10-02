015efad93208f9108904da04c63bbf8c0a6f4c27
Laurence Bugasu Ininda <laudebugs@gmail.com>
2023-10-02 14:35:14 -0400

    sth about home

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -2,7 +2,10 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-home',
-  template: ` <h1>Home</h1>`
+  template: ` <h1>Home</h1>
+  <!-- Something about home -->
+  <p>Home is where the heart is</p>
+  `
 })
 export class HomeComponent {
 
