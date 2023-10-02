c5555f8eb997e6aa524dd438072226ec9df22102
Laurence Bugasu Ininda <laudebugs@gmail.com>
2023-10-02 14:20:11 -0400

    deploy versions

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -42,21 +42,26 @@ export class UpdateService {
       )
 
       this.updates.versionUpdates
-        .pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
         .subscribe(evt => {
-          this.updateApplication();
+          if(evt.type === 'VERSION_READY'){
+
+            this.updateApplication(evt);
+          }
+          else {
+            console.log(`No updates available. Current Hash: ${evt.version} Checked at ${new Date().toUTCString()}`);
+          }
         });
 
 
       every30SecondsOnceAppIsStable$.pipe(
         mergeMap(() => appUpdates$)
       ).subscribe((appUpdate) => {
-        console.log(appUpdate);
-        this.updateApplication();
+        this.updateApplication(appUpdate);
       })
   }
 
-  updateApplication(){
-    this.matSnackBar.open('Updating to the latest version...', 'Dismiss', {duration: 30 * 1000});
+  updateApplication(appUpdate: VersionReadyEvent){
+    console.log(appUpdate);
+    this.matSnackBar.open(`Updating from ${appUpdate.currentVersion} to the latest version ${appUpdate.latestVersion}`, 'Dismiss', {duration: 30 * 1000});
   }
 }
