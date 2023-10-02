01fdb79f8be993866aca2a86945e1bf02c126d39
Laurence Bugasu Ininda <laudebugs@gmail.com>
2023-10-02 14:41:21 -0400

    another update

--- src/app/food.component.ts
+++ src/app/food.component.ts
@@ -3,7 +3,9 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-food',
-  template: ` <h1>Food</h1>`
+  template: ` <h1>Food</h1>
+  <p>Food for eating</p>
+  `
 })
 export class FoodComponent {
 
