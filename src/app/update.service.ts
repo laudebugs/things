5afada4a153059ac89f93d55171a48ffa55ed97c
Laurence Bugasu Ininda <laudebugs@gmail.com>
2023-10-03 12:02:44 -0400

    sth

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -12,6 +12,7 @@ import {
   filter,
   first,
   from,
+  interval,
   mergeMap,
   retry,
   tap,
@@ -22,43 +23,67 @@ export class UpdateService {
   constructor(
     private appRef: ApplicationRef,
     private updates: SwUpdate,
-    private snackbar: MatSnackBar
+    private matSnackBar: MatSnackBar
   ) {
     this.checkForUpdates();
+    // this.updateApplication({currentVersion: {hash: '123'}, latestVersion: {hash: '456'}} as VersionReadyEvent)
   }
 
   checkForUpdates() {
     const appIsStable$ = this.appRef.isStable.pipe(
       first((isStable) => isStable === true)
     );
+    const every30Seconds$ = interval(5 * 1000);
+    const every30SecondsOnceAppIsStable$ = concat(
+      appIsStable$,
+      every30Seconds$
+    );
 
     const update$ = from(
-      this.updates.checkForUpdate().catch(() => Promise.resolve(false))
+      this.updates.checkForUpdate().catch(() => {
+        console.log('Failed to check for update');
+        return Promise.resolve(false);
+      })
+    ).pipe(
+      tap((updateAvailable) =>
+        console.log('Update available: ', updateAvailable)
+      )
     );
 
     const versionUpdates$ = this.updates.versionUpdates.pipe(
       filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
-      catchError(() => EMPTY)
+      tap((evt) => console.log('Version update event: ', evt)),
+      catchError((error) => {
+        console.error('Error occurred while updating: ', error.message);
+        return EMPTY;
+      })
     );
 
-    const appUpdates$ = appIsStable$.pipe(
-      tap(() => console.log('App is stable')),
+    const appUpdates$ = (appIsStable$).pipe(
       mergeMap(() => update$),
-      tap((result) => console.log('Update Available: ' + result)),
       mergeMap(() => versionUpdates$),
-      tap((version) => console.log('Current version is: ' + version.currentVersion.hash)),
-      retry(1)
+      retry()
     );
 
-    appIsStable$
+    every30SecondsOnceAppIsStable$
       .pipe(mergeMap(() => appUpdates$))
       .subscribe((value) => this.updateApplication(value));
   }
 
-  updateApplication(appUpdate: VersionReadyEvent) {
-    console.log('Update Available: ' + appUpdate.currentVersion.hash + ' -> ' + appUpdate.latestVersion.hash);
-    const snack = this.snackbar.open('Update Available', 'Reload');
-
-    snack.afterDismissed().subscribe(() => window.location.reload());
+  updateApplication(appUpdate?: VersionReadyEvent) {
+    console.log(appUpdate);
+    let snackBar: MatSnackBarRef<TextOnlySnackBar>;
+    if (!appUpdate) {
+      return
+    } else {
+      snackBar = this.matSnackBar.open(
+        `Updating from ${appUpdate.currentVersion.hash} to the latest version ${appUpdate.latestVersion.hash}`,
+        'Dismiss',
+        { duration: 30 * 1000 }
+      );
+    }
+    snackBar.afterDismissed().subscribe(() => {
+      window.location.reload();
+    });
   }
 }
