cbb14bed639eb312cf40c9963860cb3e649a08e3
Laurence Bugasu Ininda <laudebugs@gmail.com>
2023-10-03 11:17:37 -0400

    update

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -12,10 +12,8 @@ import {
   filter,
   first,
   from,
-  interval,
   mergeMap,
   retry,
-  tap,
 } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
@@ -23,67 +21,39 @@ export class UpdateService {
   constructor(
     private appRef: ApplicationRef,
     private updates: SwUpdate,
-    private matSnackBar: MatSnackBar
+    private snackbar: MatSnackBar
   ) {
     this.checkForUpdates();
-    // this.updateApplication({currentVersion: {hash: '123'}, latestVersion: {hash: '456'}} as VersionReadyEvent)
   }
 
   checkForUpdates() {
     const appIsStable$ = this.appRef.isStable.pipe(
       first((isStable) => isStable === true)
     );
-    const every30Seconds$ = interval(5 * 1000);
-    const every30SecondsOnceAppIsStable$ = concat(
-      appIsStable$,
-      every30Seconds$
-    );
 
     const update$ = from(
-      this.updates.checkForUpdate().catch(() => {
-        console.log('Failed to check for update');
-        return Promise.resolve(false);
-      })
-    ).pipe(
-      tap((updateAvailable) =>
-        console.log('Update available: ', updateAvailable)
-      )
+      this.updates.checkForUpdate().catch(() => Promise.resolve(false))
     );
 
     const versionUpdates$ = this.updates.versionUpdates.pipe(
       filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
-      tap((evt) => console.log('Version update event: ', evt)),
-      catchError((error) => {
-        console.error('Error occurred while updating: ', error.message);
-        return EMPTY;
-      })
+      catchError(() => EMPTY)
     );
 
-    const appUpdates$ = (appIsStable$).pipe(
+    const appUpdates$ = appIsStable$.pipe(
       mergeMap(() => update$),
       mergeMap(() => versionUpdates$),
-      retry()
+      retry(1)
     );
 
-    every30SecondsOnceAppIsStable$
+    appIsStable$
       .pipe(mergeMap(() => appUpdates$))
       .subscribe((value) => this.updateApplication(value));
   }
 
-  updateApplication(appUpdate?: VersionReadyEvent) {
-    console.log(appUpdate);
-    let snackBar: MatSnackBarRef<TextOnlySnackBar>;
-    if (!appUpdate) {
-      return
-    } else {
-      snackBar = this.matSnackBar.open(
-        `Updating from ${appUpdate.currentVersion.hash} to the latest version ${appUpdate.latestVersion.hash}`,
-        'Dismiss',
-        { duration: 30 * 1000 }
-      );
-    }
-    snackBar.afterDismissed().subscribe(() => {
-      window.location.reload();
-    });
+  updateApplication(appUpdate: VersionReadyEvent) {
+    const snack = this.snackbar.open('Update Available', 'Reload');
+
+    snack.afterDismissed().subscribe(() => window.location.reload());
   }
 }
