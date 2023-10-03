f3002104d169f71887e7067f1cb2373d319db05c
Laurence Bugasu Ininda <laudebugs@gmail.com>
2023-10-03 13:25:38 -0400

    edit beach

--- src/app/beach.component.ts
+++ src/app/beach.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-beach',
   template: ` <h1>Beach</h1>
-  <p>Beach for relaxing</p>
+  <p>Beach for relaxing!</p>
   <img width="300" src="https://images.unsplash.com/photo-1520454974749-611b7248ffdb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1587&q=80" alt="">
   `
 })
