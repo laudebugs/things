1d843d21ac6d885d09b36b8c27f44874a40d9366
Laurence Bugasu Ininda <laudebugs@gmail.com>
2023-10-03 11:07:30 -0400

    more

---
+++ src/app/mountain.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-mountain',
+  template: ` <h1>Mountain</h1>
+  <p>Mountain for climbing</p>
+  <img width="300" src="https://images.unsplash.com/photo-1508923567004-3a6b8004f3d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1vdW50YWlufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="">
+  `
+})
+export class MountainComponent {
+
+  }
